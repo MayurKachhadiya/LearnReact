2030bbf3dc19be55923ced0dbf29bacf68ff6c04
Mayur Kachhadiya <mayurkachhadiya373@gmail.com>
2022-05-31 10:28:39 +0530

    WE ARE LEARN REACT

--- Form.jsx
+++ Form.jsx
@@ -1,31 +1,41 @@
 import React,{useState} from "react";
 
 const Form = () =>{
-   const [name, setname] = useState("");
-   const [useName, setuseName] = useState();
-   const [lastName, setlastName] = useState("");
-const [lastFullName, setlastFullName] = useState();
+   const [FullName, setFullName] = useState({
+        fname: '',
+        lname: '',
+        ename: '',
+        phone: '',
+    });
 
-   const lastNewName = (e)=>{
-       console.log(e.target.value);
-       setlastName(e.target.value);
-   }
     const inputName = (e)=>{
         console.log(e.target.value);
-        setname(e.target.value);
-    }
+        console.log(e.target.name);
+
+        const value = e.target.value;
+        const name = e.target.name;
+        setFullName((preValue)=>{
+           return{
+               ...preValue,
+               [name]:value,
+        };
+        });
+       
+    };
+
     const submit = (e)=>{
         e.preventDefault();
-        setuseName(name);
-        setlastFullName(lastName);
+        alert('Form submited');
     }
     return(
         <>
             <form onSubmit={submit}>
             <div>
-            <h1>Hello {useName} {lastFullName}</h1>
-            <input type='text' placeholder='Enter Your Name' onChange={inputName} value={name}/><br/>
-            <input type='text' placeholder='Enter Your LastName' onChange={lastNewName} value={lastName}/><br/>
+            <h1>Hello {FullName.fname} {FullName.lname} {FullName.ename} {FullName.phone} </h1>
+            <input type='text' name="fname" placeholder='Enter Your Name' onChange={inputName} value={FullName.fname}/><br/>
+            <input type='text' name="lname" placeholder='Enter Your LastName' onChange={inputName} value={FullName.lname}/><br/>
+            <input type='text' name="ename" placeholder='Enter Your Email' onChange={inputName} value={FullName.ename}/><br/>
+            <input type='number' name="phone" placeholder='Enter Your phone' onChange={inputName} value={FullName.phone}/><br/>
             <button>Click here 👍</button>
             </div>
             </form>
